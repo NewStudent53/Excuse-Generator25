da78353909926b703d43f75fe9a98e79f64890d0
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2024-04-03 22:57:14 -0400

    Update index.js
    upload

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-window.onload = () => {
-    document.querySelector('#btn').addEventListener("click", () => {
-        document.querySelector('#the-excuse').innerHTML = excuseGen();
-    });
-    console.log('Hello Rigo from the console!');
-};
+function excuseGen() {
 
-let excuseGen = () => {
-    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-    let action = ['ate', 'peed', 'crushed', 'broke'];
-    let what = ['my homework', 'my phone', 'the car'];
-    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    var who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    var action = ['ate', 'peed', 'crushed', 'broke'];
+    var what = ['my homework', 'my phone', 'the car'];
+    var when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-    let whoIndx = Math.floor(Math.random() * who.length);
-    let actionIndx = Math.floor(Math.random() * action.length);
-    let whatIndx = Math.floor(Math.random() * what.length);
-    let whenIndx = Math.floor(Math.random() * when.length);
+    var C1 = who[Math.round(Math.random()*(Character.length-1))];
+    var C2 = action[Math.round(Math.random()*(Action.length-1))];
+    var C3 = what[Math.round(Math.random()*(Object.length-1))];
+    var C4 = when[Math.round(Math.random()*(Object.length-1))];
 
-    return who[whoIndx] + ' ' + action[actionIndx] + ' ' + what[whatIndx] + ' ' + when[whenIndx];
-};
+    document.getElementById('TheExcuses').innerHTML = '<div>' C1 + ' ' + C2 + ' ' + C3 + ' ' + C4 '</div>'
+}
