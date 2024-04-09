f75021f234f2ff4259cd77230142477bc2ab356c
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2024-04-08 23:04:46 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function excuseGen() {
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    var who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    var action = ['ate', 'peed', 'crushed', 'broke'];
+    var what = ['my homework', 'my phone', 'the car'];
+    var when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    var C1 = who[Math.round(Math.random()*(who.length-1))];
+    var C2 = action[Math.round(Math.random()*(action.length-1))];
+    var C3 = what[Math.round(Math.random()*(what.length-1))];
+    var C4 = when[Math.round(Math.random()*(when.length-1))];
+
+	document.getElementById('TheExcuses').innerHTML = C1 + ' ' + C2 + ' ' + C3 + ' ' + C4
+}
