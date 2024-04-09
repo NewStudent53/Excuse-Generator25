ac24aa4ba0e94dd1eee92473a2dea3cf0c66e7eb
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2024-04-08 20:56:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function excuseGen() {
     var what = ['my homework', 'my phone', 'the car'];
     var when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-    var C1 = who[Math.round(Math.random()*(Character.length-1))];
-    var C2 = action[Math.round(Math.random()*(Action.length-1))];
-    var C3 = what[Math.round(Math.random()*(Object.length-1))];
-    var C4 = when[Math.round(Math.random()*(Object.length-1))];
+    var C1 = who[Math.round(Math.random()*(who.length-1))];
+    var C2 = action[Math.round(Math.random()*(action.length-1))];
+    var C3 = what[Math.round(Math.random()*(what.length-1))];
+    var C4 = when[Math.round(Math.random()*(when.length-1))];
 
-    document.getElementById('TheExcuses').innerHTML = '<div>' C1 + ' ' + C2 + ' ' + C3 + ' ' + C4 '</div>'
+	document.getElementById('TheExcuses').innerHTML = C1 + ' ' + C2 + ' ' + C3 + ' ' + C4
 }
